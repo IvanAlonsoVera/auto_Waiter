7bf02cffe5ace25deb40cd004a96c56ad1489795
Torre <thevtk99@gmail.com>
2022-05-24 20:48:38 +0200

    240522 cambiosController

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class PedidosController extends Controller
 {
-    //
+    //Generar pedido en la bbdd
+                        //tiene que recibir un parámetro con los IDs de los platos, precios y  las cantidades en el orden correspondiente
+    public function crearPedidoEnBBDD(Request $datos){
+        //se va a generar al darle a comprar
+        $pedido = new pedidos();
+        
+                            //esto debería ser una cadena de texto separado por comas o puntos(para no liar a la bbdd)
+        $p->productos = $datos-> productos;
+        $p->cantidades = $datos-> cantidades;
+        $p->precio = $datos-> precio;
+
+        $p->save();
+    
+        
+
+        //en el return darle la cantidad de platos añadidos a la cesta?
+    }
+
+    //sacar carrito
+
+    public function listarPedido(){
+        //hay que buscar el pedido según el ID. En la consulta traer el nombre
+        //$lp = pedidos::All();
+
+        return view('listarPedido',['listarPedido' => $lp]);
+    }
 }
--- app/Http/Controllers/PlatoController.php
+++ app/Http/Controllers/PlatoController.php
@@ -8,72 +8,7 @@ use App\Models\Pedidos;
 
 
 class PlatoController extends Controller
-{                   //
-    public function listadoPlatos($id){
-        $listaPlatos = Plato::where('pedido_id',$id)->get();
-            //listaPlato recogerá los datos y los pasará al blade
-        return view('listaPlatos',['listaPlato' => $listaPlatos]);
-    }
+{                   
 
-
-    public function formCrear(){
-        return view('formularioCrearPlato',['listaPedidos'=>pedido::All()]);
-    }
-
-    public function guardar(Request $datos){
-        //validar
-        $respuesta = $datos->validate([
-            //cuidaooo hay que ver qué queremos
-            'nombre' => 'required',
-            'precio' => 'required|numeric',
-            'pedido_id'=>'required'
-        ]);
-        //crear modelo
-        $platoParametro = new Plato();
-
-        $platoParametro->nombre = $datos->nombre;
-        $platoParametro->edad = $datos->edad;
-        $platoParametro->pedido_id = $datos->pedido_id;
-
-        $platoParametro->save();
-            //utilizamos el nombre de la ruta. También necesita parámetro en este caso
-        return redirect()->route('listaPlatos',['id'=>$datos->pedido_id]);
-    }
-
-
-
-    public function borrar($id){
-
-        $platoParametro = Plato::find($id);
-
-        $platoParametro->delete();
-
-        $pedido_id  =$platoParametro->pedido_id;
-       return redirect()->route('listaPlatos',['id'=>$pedido_id]);
-    
-    }
-
-
-    public function modificar($id){
-
-        $platoParametro = Plato::find($id);
-
-        $pedidos = pedido::All();
-
-       return view('formularioCrearPlato',['Plato'=>$platoParametro,'listaPedidos'=>$pedidos]);
-    
-    }
-
-    public function modificarGuardarBD(Request $datos, $id){
-        $platoParametro = Plato::find($id);
-
-        $platoParametro->nombre = $datos->nombre;
-        $platoParametro->edad = $datos->precio;
-        $platoParametro->pedido_id = $datos->pedido_id;
-
-        $platoParametro->save();
-
-        return redirect()->route('dashboard');
-    }
 
 }
