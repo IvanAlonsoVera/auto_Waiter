0d250e25359a458bf94dfdac1da5a913183f9e6e
Alejandro Torrecillas <thevtk99@gmail.com>
2022-06-09 10:58:05 +0200

    090622 ModificacionesCarrito_v1

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,7 +32,7 @@
                             </div>
                             @php $total = 0 @endphp
                             @foreach((array) session('cart') as $id => $details)
-                                @php $total += $details['price'] * $details['quantity'] @endphp
+                                @php $total += $details['price'] * $details['cantidad'] @endphp
                             @endforeach
                             <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
                                 <p>Total: <span class="text-info">$ {{ $total }}</span></p>
@@ -42,11 +42,11 @@
                             @foreach(session('cart') as $id => $details)
                                 <div class="row cart-detail">
                                     <div class="col-lg-4 col-sm-4 col-4 cart-detail-img">
-                                        <img src="{{ $details['image'] }}" />
+                                        <img src="{{ $details['img'] }}" />
                                     </div>
                                     <div class="col-lg-8 col-sm-8 col-8 cart-detail-product">
-                                        <p>{{ $details['name'] }}</p>
-                                        <span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> Quantity:{{ $details['quantity'] }}</span>
+                                        <p>{{ $details['nombre'] }}</p>
+                                        <span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> cantidad:{{ $details['cantidad'] }}</span>
                                     </div>
                                 </div>
                             @endforeach
